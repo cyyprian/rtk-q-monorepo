227fa80dd5b17a4298d9e0983d961b6dd32d0af8
cyyprian <cyyprian@icloud.com>
2026-01-31 05:54:24 +0200

    added types in registerAuthEndpoints

--- packages/auth/registerAuthEndpoints.ts
+++ packages/auth/registerAuthEndpoints.ts
@@ -1,16 +1,34 @@
+import type {
+    Api,
+    EndpointDefinitions,
+    ApiModules,
+    fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+
 export interface AuthUser {
-    name: string | null
-    email: string | null
+    name: string | null;
+    email: string | null;
 }
 
-export const registerAuthEndpoints = (api) => {
+export const registerAuthEndpoints = <
+    Definitions extends EndpointDefinitions,
+    ReducerPath extends string,
+    TagTypes extends string,
+    Enhancers extends keyof ApiModules<any, any, any, any>,
+>(
+    api: Api<
+      ReturnType<typeof fetchBaseQuery>,
+      Definitions,
+      ReducerPath,
+      TagTypes,
+      Enhancers
+    >,
+) => {
     return api.injectEndpoints({
         endpoints: (builder) => ({
             user: builder.query<AuthUser, void>({
-                query: () => '/auth/me'
+                query: () => "/auth/me",
             }),
-        })
+        }),
     });
-}
-
-// export const { useUserQuery } = registerAuthEndpoints;
+};
