91cce032ec8e34dccc5f1c33b7b56af4749aa431
cyyprian <cyyprian@icloud.com>
2026-02-05 15:49:41 +0200

    log api instance

--- apps/example/src/store/sharedAuth.ts
+++ apps/example/src/store/sharedAuth.ts
@@ -1,6 +1,12 @@
 import { registerAuthEndpoints } from "@packages/auth";
 import api from "./api";
 
+console.log('api', api);
+
 const authApi = registerAuthEndpoints(api);
+
+console.log('authApi', authApi);
+
+console.log('is \'authApi\' instance of \'api\'', authApi === api);
 
 export const { useUserQuery, useLoginMutation } = authApi;
